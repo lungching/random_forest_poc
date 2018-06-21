d18eef1c81b4c4d5d7bb6bf2268f4f5aa95f98bd
lungching <mburns.lungching@gmail.com>
2018-06-21 11:00:55 -0400

    add data and script to run random forest on random forests

---
+++ guess_forest.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require_relative 'src/random_forest.rb'
+require 'csv'
+
+train_data = CSV.read('forest_data.csv')
+header = train_data.shift
+
+forest = RandomForest.new(num_of_trees: 50)
+forest.create_forest(train_data)
+# forest.forest[0].pretty_print()
+
+# should be Umpqua National Forest
+# prediction = forest.winner(
+#  [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,1,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0]
+# )
+
+# should be Tuskegee National Forest
+prediction = forest.winner(
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+)
+
+puts "Is forest #{prediction}"
